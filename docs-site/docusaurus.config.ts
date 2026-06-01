97f9a5bf03d8ecc03e0b758bb39593d69918aa14
wangsn <wangsn@tuya.com>
2026-06-01 11:12:49 +0800

    Revert "modify url and baseUrl"
    This reverts commit 3f2ec3b0890dc995a5d617fcb92a89b3d3e4cab5.

--- docs-site/docusaurus.config.ts
+++ docs-site/docusaurus.config.ts
@@ -11,8 +11,8 @@ const config: Config = {
     v4: true,
   },
 
-  url: 'https://agentic-kit.tuya.com/',
-  baseUrl: '/',
+  url: 'https://sedawwk.github.io',
+  baseUrl: '/myprivaterepo/',
 
   organizationName: 'sedawwk',
   projectName: 'myprivaterepo',
